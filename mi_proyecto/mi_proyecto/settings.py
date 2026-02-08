d4b9023c62f4dab583f6c916d575d29b4a2c9b0b
papipollo <damarisdanielanaranjo@gmail.com>
2026-02-08 14:15:10 -0500

    avance_django

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mi_app',   # 👈 tu app
 ]
 
 MIDDLEWARE = [
@@ -102,11 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
